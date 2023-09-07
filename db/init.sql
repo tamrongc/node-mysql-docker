bd6e3722f4d7e1f474df89b2be90a7973f93aaff
Tamrong Chaiwichit <tamrong.c@ayodiacompany.com>
2023-09-07 14:13:07 +0700

    update volum and initial data

--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,6 @@
-CREATE TABLE Student(
-	student_id INT PRIMARY KEY AUTO_INCREMENT,
-	student_name VARCHAR(60),
-	student_age INT
+CREATE TABLE IF NOT EXISTS Counters(
+	counter_id INT PRIMARY KEY AUTO_INCREMENT
 );
-
-INSERT INTO Student(student_name, student_age) VALUES("Shubham verma", 21);
-INSERT INTO Student(student_name, student_age) VALUES("Utkarsh Rickey", 23);
-INSERT INTO Student(student_name, student_age) VALUES("Gerard verma", 25);
-INSERT INTO Student(student_name, student_age) VALUES("Donald Alfredo", 22);
-INSERT INTO Student(student_name, student_age) VALUES("Roman Elias", 19);
-INSERT INTO Student(student_name, student_age) VALUES("Rickey Joe", 20);
-INSERT INTO Student(student_name, student_age) VALUES("Stanley Jamie", 21);
-INSERT INTO Student(student_name, student_age) VALUES("Dwayne Jamie", 22);
-INSERT INTO Student(student_name, student_age) VALUES("Jan Shawn", 22);
-INSERT INTO Student(student_name, student_age) VALUES("Cameron verma", 21);
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 
