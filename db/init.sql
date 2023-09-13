307d52ba7876f08b2aa813019306f04cbf4538bf
Tamrong Chaiwichit <tamrong.c@ayodiacompany.com>
2023-09-13 13:52:31 +0700

    change database to mariaDb

--- db/init.sql
+++ db/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS Counters(
-	counter_id INT PRIMARY KEY AUTO_INCREMENT
+	counter_id BIGINT PRIMARY KEY AUTO_INCREMENT
 );
 
 ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
--- web/server.js
+++ web/server.js
@@ -1,49 +1,68 @@
 const express = require('express');
-const mysql = require('mysql');
+const mariadb = require('mariadb');
 const app = express();
 app.use(express.static('public'));
 
-const connection = mysql.createPool({
-	connectionLimit: 10,
+const pool = mariadb.createPool({
 	host: process.env.MYSQL_HOST || 'localhost',
 	user: process.env.MYSQL_USER || 'root',
 	password: process.env.MYSQL_PASSWORD || 'password',
-	database: process.env.MYSQL_DATABASE || 'test'
+	database: process.env.MYSQL_DATABASE || 'test',
+	bigIntAsNumber:true,
+	connectionLimit: 10,
+	acquireTimeout: 30000
 });
 
 app.listen(5000, () => console.log('listining on port 5000'));
 app.get('/', (req, res) => { res.sendFile(__dirname + '/public/index.html'); });
 
-app.get('/count', (req, res) => {
-	connection.query('SELECT COUNT(*) as count FROM Counters;' , (err, result) => {
-		if(err){
-			res.send(err);
+app.get('/count', async (req, res) => {
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		let query = "SELECT COUNT(*) as count FROM Counters;";
+		let result = await connection.query(query);
+		res.send(result);
+	}
+	catch (err) {
+		res.send(err);
+	} finally {
+		if (connection) {
+			connection.end();
 		}
-		else{
-			res.send(result);
-		}
-	});
+	}
 });
 
-app.post('/click-up', (req, res) => {
-	connection.query('INSERT INTO Counters VALUE(NULL);' , (err, result) => {
-		if(err){
-			res.send(err);
+app.post('/click-up', async (req, res) => {
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		let query = "INSERT INTO Counters VALUE(NULL);";
+		await connection.query(query);
+		res.sendStatus(200);
+	}
+	catch (err) {
+		res.send(err);
+	} finally {
+		if (connection) {
+			connection.end();
 		}
-		else{
-			res.sendStatus(200);
-		}
-	});
-  });
+	}
+});
 
-  app.delete('/click-down', (req, res) => {
-	connection.query('DELETE FROM Counters LIMIT 1;' , (err, result) => {
-		if(err){
-			res.send(err);
+app.delete('/click-down', async (req, res) => {
+	let connection;
+	try {
+		connection = await pool.getConnection();
+		let query = "DELETE FROM Counters LIMIT 1;";
+		await connection.query(query);
+		res.sendStatus(200);
+	}
+	catch (err) {
+		res.send(err);
+	} finally {
+		if (connection) {
+			connection.release();
 		}
-		else{
-			res.sendStatus(200);
-		}
-	});
-  });
-  
+	}
+});
