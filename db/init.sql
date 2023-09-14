618357ad01c2b642dec8579519da8d5a3804e109
Tamrong Chaiwichit <tamrong.c@ayodiacompany.com>
2023-09-14 09:07:09 +0700

    แก้ไข docker compose

--- db/init.sql
+++ db/init.sql
@@ -1,7 +1,9 @@
+CREATE DATABASE IF NOT EXISTS test2;
+
+USE test2;
+
 CREATE TABLE IF NOT EXISTS Counters(
 	counter_id BIGINT PRIMARY KEY AUTO_INCREMENT
 );
-
-ALTER USER 'root' IDENTIFIED WITH mysql_native_password BY 'password';
 
 flush privileges;
